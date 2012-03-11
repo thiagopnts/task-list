1577eb070c9d71bb7442132e85cd9441a9ae251b
Thiago Pontes <thiagopnts@gmail.com>
2012-03-11 14:41:28 -0300

    Refactored some tests.

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -25,28 +25,38 @@ describe 'TaskListApp' do
         params = {:name=>"blah", :done=>false}
         request "/tasks", :method => :post, :params=> params.to_json
         last_response.should be_ok
+        @task = Task.first(:name => 'blah')
+        @task.name.should == "blah"
+        @task.done.should == false
+        @task.id.should == ""
     end
 
     it "should receive all tasks" do
-        request "/tasks", :method => :get
+        request "/tasks", :method => :get 
         last_response.body.empty? == false
     end
 
     it "should create and updates tasks" do
-        @task = Task.create(:name=>"do more tests", :done => true)
+        @task = Task.create(:name=>"do more tests", :done => false)
         @task.serial.should_not == 0
         @task.name.should == 'do more tests'
-        @task.done.should == true
+        @task.done.should == false
         @task.id = "123123"
         @task.save
         params = {:id=>123123, :name=>"do more tests (Done)", :done=> true}
         request "/tasks", :method => :put, :params=> params.to_json
+        @task = Task.first(:id=>"123123")
         last_response.should be_ok
+        @task.name.should == "do more tests (Done)"
+        @task.done.should == true
     end
 
     it "should delete tasks" do
         params = {"id"=>123123}
         request "/tasks", :method => :delete, :params=>params
+        @task = Task.first(:id => "123123")
+        @task.should == nil
+        
     end
 
     it "should not have more tasks" do
